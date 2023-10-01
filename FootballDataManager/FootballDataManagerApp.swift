70a842535a4de5bc980688d8c10a9bc7252bdb88
Keisuke2162 <applabo.kei@gmail.com>
2023-10-01 21:14:47 +0900

    Initial Commit

---
+++ FootballDataManager/FootballDataManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballDataManagerApp.swift
+//  FootballDataManager
+//
+//  Created by Kei on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct FootballDataManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
