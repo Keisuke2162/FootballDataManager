d8fc7bc60c5d4e92ac34f96a3256ef199d794160
Keisuke Ueda <42376257+Keisuke2162@users.noreply.github.com>
2023-10-06 05:05:45 +0900

    順位表実装 (#1)
    * フォント実装、順位表修正
    
    * 勝ち点表示
    
    * Homeの背景色変更

--- FootballDataManager/Standing/StandingView.swift
+++ FootballDataManager/Standing/StandingView.swift
@@ -10,28 +10,60 @@ import SwiftUI
 struct StandingView: View {
     let leagueID: String
     @ObservedObject private var viewModel = StandingViewModel()
+    @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        VStack {
-            List {
-                ForEach(0 ..< viewModel.list.count, id: \.self) { index in
-                    HStack(spacing: 16.0) {
-                        let imageURL = URL(string: viewModel.list[index].team.logo)
-                        AsyncImage(url: imageURL) { image in
-                            image.resizable()
-                        } placeholder: {
-                            ProgressView()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(0 ..< viewModel.list.count, id: \.self) { index in
+                        HStack(spacing: 16.0) {
+                            Text("\(index + 1)")
+                                .foregroundColor(Color.white)
+                                .font(.custom("SSportsD-Medium", size: 16))
+                                .frame(width: 24)
+                            let imageURL = URL(string: viewModel.list[index].team.logo)
+                            AsyncImage(url: imageURL) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            Text(viewModel.list[index].team.name)
+                                .foregroundColor(Color.white)
+                                .font(.custom("SSportsD-Medium", size: 16))
+                            Spacer()
+                            Text(viewModel.list[index].points.description)
+                                .foregroundColor(Color.white)
+                                .font(.custom("SSportsD-Medium", size: 16))
                         }
-                        .scaledToFit()
-                        .frame(width: 30, height: 30)
-                        Text(viewModel.list[index].team.name)
+                        .frame(height: 46)
+                        .listRowBackground(Color.clear)
                     }
-                    .frame(height: 46)
                 }
+                .scrollContentBackground(.hidden)
+                .background(Color.init("SkySportsBlue"))
+                .listStyle(.grouped)
+            }
+            .onAppear {
+                viewModel.getTableList(leagueID)
             }
         }
-        .onAppear {
-            viewModel.getTableList(leagueID)
+        .navigationBarBackButtonHidden(true)
+        .toolbarBackground(.white, for: .navigationBar)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    HStack {
+                        Image(systemName: "chevron.backward")
+                            .font(.system(size: 17, weight: .medium))
+                    }
+                    .foregroundColor(.white)
+                }
+            }
         }
     }
 }
