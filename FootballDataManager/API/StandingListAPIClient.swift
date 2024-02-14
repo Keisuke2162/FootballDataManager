2d63c8332ff27d6b4492df4304986ad4759f3d32
Keisuke2162 <applabo.kei@gmail.com>
2024-02-15 02:25:58 +0900

    StandingClient実装

--- FootballDataManager/API/StandingListAPIClient.swift
+++ FootballDataManager/API/StandingListAPIClient.swift
@@ -14,10 +14,58 @@ struct StandingClient {
 }
 
 extension StandingClient: TestDependencyKey {
-    static let previewValue: Self(
-        
+    static let previewValue = Self(
+        getStanding: { _ in [.mock] }
+    )
+    static var testValue: StandingClient = Self()
+}
+
+extension DependencyValues {
+    var standingClient: StandingClient {
+        get { self[StandingClient.self] }
+        set { self[StandingClient.self] = newValue }
+    }
+}
+
+extension StandingClient: DependencyKey {
+    static let liveValue: StandingClient = StandingClient(
+        getStanding: { id in
+            guard let apiURL = URL(string: "https://v3.football.api-sports.io/standings") else {
+                throw APIError.invalidURL
+            }
+
+            var urlComponents = URLComponents(url: apiURL, resolvingAgainstBaseURL: true)
+            urlComponents?.queryItems = [
+                .init(name: "season", value: "2023"),
+                .init(name: "league", value: id)
+            ]
+
+            guard let url = urlComponents?.url else {
+                throw APIError.invalidURL
+            }
+
+            var request = URLRequest(url: url)
+            request.setValue("", forHTTPHeaderField: "x-rapidapi-key")
+            request.httpMethod = "GET"
+            
+            guard let fileURL = Bundle.main.url(forResource: "football_api_standings_2023_39", withExtension: "json") else {
+                throw APIError.unknown
+            }
+
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let item = try JSONDecoder().decode(StandingsItem.self, from: data)
+                guard let items = item.response.first?.league.standings.first else {
+                    throw APIError.unknown
+                }
+                return items
+            } catch {
+                throw APIError.unknown
+            }
+        }
     )
 }
+
 
 final class StandingListAPIClient {
     func getStanding(leagueID: String) async throws -> [Standing] {
