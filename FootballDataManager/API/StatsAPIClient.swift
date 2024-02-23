c27e465a95e058967eea89315f56652e7ab7484c
Keisuke2162 <applabo.kei@gmail.com>
2024-02-23 20:38:57 +0900

    APIClient, Entity, JSONファイル追加

---
+++ FootballDataManager/API/StatsAPIClient.swift
@@ -0,0 +1,78 @@
+//
+//  StatsAPIClient.swift
+//  FootballDataManager
+//
+//  Created by Kei on 2024/02/21.
+//
+
+import ComposableArchitecture
+import Foundation
+
+@DependencyClient
+struct StatsAPIClient {
+    var getTopScorers: @Sendable (_ type: LeagueType) async throws -> FixturesItem
+}
+
+extension StatsAPIClient: TestDependencyKey {
+//    static let previewValue = Self(
+//        getFixtures: { _ in .mock }
+//    )
+    static let previewValue = Self(
+        getTopScorers: { type in
+            do {
+                return try await liveValue.getTopScorers(type)
+            } catch { return .init(response: []) }
+        }
+    )
+    static let testValue: StatsAPIClient = Self()
+}
+
+extension DependencyValues {
+    var statsAPIClient: StatsAPIClient {
+        get { self[StatsAPIClient.self] }
+        set { self[StatsAPIClient.self] = newValue }
+    }
+}
+
+extension StatsAPIClient: DependencyKey {
+    static let liveValue: StatsAPIClient = StatsAPIClient(
+        getTopScorers: { type in
+            var components = URLComponents(string: "https://v3.football.api-sports.io/fixtures")!
+            //https://v3.football.api-sports.io/fixtures?season=2021&league=39
+            components.queryItems = [
+                .init(name: "season", value: "2023"),
+                .init(name: "league", value: type.id)
+            ]
+
+            // MARK: - API Request
+//            var request = URLRequest(url: components.url!)
+//            request.setValue("", forHTTPHeaderField: "x-rapidapi-key")
+//            request.httpMethod = "GET"
+//
+//            let (data, _) = try await URLSession.shared.data(from: request.url!)
+//            do {
+//                let item = try JSONDecoder().decode(FixturesItem.self, from: data)
+//                return item
+//            } catch {
+//                throw APIError.unknown
+//            }
+            
+            // MARK: - Local JSON File
+            guard let fileURL = Bundle.main.url(forResource: type.fixturesResource, withExtension: "json") else {
+                throw APIError.unknown
+            }
+
+            do {
+                let data = try Data(contentsOf: fileURL)
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                let item = try decoder.decode(FixturesItem.self, from: data)
+                return item
+            } catch {
+                throw APIError.unknown
+            }
+        }
+    )
+}
---
+++ FootballDataManager/Views/Stats/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  FootballDataManager
+//
+//  Created by Kei on 2024/02/21.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
