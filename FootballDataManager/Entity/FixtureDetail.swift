2092c70f631410a60be6d5558bd24c74d31b15a0
Keisuke2162 <applabo.kei@gmail.com>
2024-02-25 23:05:38 +0900

    Entity実装

--- FootballDataManager/Entity/FixtureDetail.swift
+++ FootballDataManager/Entity/FixtureDetail.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+struct FixtureDetailItem: Codable {
+    let response: FixtureDetail
+}
+
+struct FixtureDetail: Codable, Equatable, Identifiable, Sendable {
+    static func == (lhs: FixtureDetail, rhs: FixtureDetail) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    var id: Int {
+        team.id
+    }
+    let team: FixtureTeam
+    let statistics: [Statictics]
+}
+
+struct Statictics: Codable {
+    let type: StatisticType
+    let value: String?
+}
+
+enum StatisticType: String, Codable {
+    case shotsOnGoal = "Shots on Goal"
+    case shotsOffGoal = "Shots off Goal"
+    case totalShots = "Total Shots"
+    case blockedShots = "Blocked Shots"
+    case shotsInsideBox = "Shots insidebox"
+    case shotsOutsideBox = "Shots outsidebox"
+    case fouls = "Fouls"
+    case cornerKicks = "Corner Kicks"
+    case offsides = "Offsides"
+    case ballPossession = "Ball Possession"
+    case yellowCards = "Yellow Cards"
+    case redCards = "Red Cards"
+    case goalkeeperSaves = "Goalkeeper Saves"
+    case totalPasses = "Total passes"
+    case passesAccurate = "Passes accurate"
+    case passesPercentage = "Passes %"
+    case expectedGoals = "expected_goals"
+}
